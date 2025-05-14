8de28889da8dffb9bbc111a56485d16d48e13817
AmrKhaled35 <amrk6965@gmail.com>
2025-05-14 19:35:15 +0300

    Add : Push Function in Stack Class

--- main.cpp
+++ main.cpp
@@ -3,6 +3,29 @@
 using namespace std;
 typedef long long ll;
 template<typename T>
+class Stack {
+    struct node {
+        T Item;
+        node *Next;
+    };
+    node *top;
+public:
+    Stack() {
+        top = NULL;
+    }
+    void push(T newItem) {
+        node *newNode = new node;
+        if (newNode == NULL) {
+            cout << "Stack Overflow" << endl;
+        }
+        else {
+            newNode->Item = newItem;
+            newNode->Next = top;
+            top = newNode;
+        }
+    }
+
+};
 
 int main() {
 
